f637fe206948b74792448841f1c49a66a22ebf1c
ErickBasto <erickemmanuelbastocen@gmail.com>
2023-04-18 16:39:13 -0600

    Modificación del componente Counter

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,6 @@
-<div>
-    <h1 class="text-white">Hello World!</h1>
+<div style="text-align: center">
+    <CENTER>
+    <button wire:click="increment" class="text-white">+</button>
+    <h1 class="text-white">{{ $count }}</h1>
+    </CENTER>
 </div>
